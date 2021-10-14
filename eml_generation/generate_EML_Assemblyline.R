4a9aac0272022fff024779bb212d2a6d2b08e975
camilavargasp <cvargas@bren.ucsb.edu>
2021-10-14 16:13:41 -0700

    updating code file path to clean_data folder

--- eml_generation/generate_EML_Assemblyline.R
+++ eml_generation/generate_EML_Assemblyline.R
@@ -15,7 +15,7 @@ if (length(entity[entity$entitytype=="otherEntity","filename"])!=0) {otherentity
 #create a template
 eal_inputs <- EMLassemblyline::template_arguments(
   empty = T, 
-  data.path = project_path, 
+  data.path = paste0(getwd(), "/clean_data"), #project_path, 
   data.table = if (datatable_present==1) {entity[entity$entitytype=="dataTable","filename"]} else {NULL},
   other.entity = if (otherentity_present==1){entity[entity$entitytype=="otherEntity","filename"]} else {NULL}
 )
@@ -23,8 +23,8 @@ eal_inputs <- EMLassemblyline::template_arguments(
 #dataset level
 eal_inputs$dataset.title <- excel_input$dataset$title
 eal_inputs$data.path = eal_inputs$eml.path <- project_path
-#eal_inputs$data.path <- paste0(getwd(), "/clean_data")
-eal_inputs$eml.path <- project_path
+eal_inputs$data.path <- paste0(getwd(), "/clean_data")
+#eal_inputs$eml.path <- project_path
 eal_inputs$maintenance.description <- 'Completed'
 eal_inputs$package.id <- excel_input$dataset$packageid
 eal_inputs$user.domain <- excel_input$dataset$scope
