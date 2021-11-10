742bdab54e27823fe3fbcd9b2a9fd9375068e14d
camilavargasp <cvargas@bren.ucsb.edu>
2021-11-10 11:10:23 -0800

    adding metadata folder url and adding some folder numbers to file paths

--- 4.project.XX/project.XX.R
+++ 4.project.XX/project.XX.R
@@ -18,15 +18,15 @@ drive_find(n_max = 10)
 # change the following number based on each of the datasets
 dataset_id <- 101
 
-folder_path<- here::here("project.101/")
+folder_path<- here::here(paste0("4.project.", dataset_id,"/"))
 
 #end user edit zone###########
 ############################################################
 
 #loading all the functions
-source(paste0(getwd(), '/eml_generation/download_metadata.R'))
-source(paste0(getwd(), '/eml_generation/get_meta_xlsx.R'))
-source(paste0(getwd(), '/eml_generation/generate_EML_Assemblyline.R'))
+source(paste0(getwd(), '/5.eml_generation/download_metadata.R'))
+source(paste0(getwd(), '/5.eml_generation/get_meta_xlsx.R'))
+source(paste0(getwd(), '/5.eml_generation/generate_EML_Assemblyline.R'))
 
 
 #read the metadata content out of xlsx
--- 5.eml_generation/download_metadata.R
+++ 5.eml_generation/download_metadata.R
@@ -1,7 +1,7 @@
 #### Scripts that downloads the metadata spreadsheet to the project's folder
 
 ##Url where the metadata sheet lives
-metadata_folder_url <- " "
+metadata_folder_url <- "https://drive.google.com/drive/u/0/folders/1kG14ZSwvdf1Jc1owZ1vZPXDJW2x4UYpl"
 
 ## Identifying the file
 metadata_file <- drive_ls(as_id(metadata_folder_url))
